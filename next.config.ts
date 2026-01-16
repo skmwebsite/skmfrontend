abb1f9e02f813e09b1901c03253fbdcadbaa0e57
afl-h <58439230+afl-h@users.noreply.github.com>
2026-01-16 16:31:56 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,7 @@
 /** @type {import("next").NextConfig} */
 const nextConfig = {
   images: {
-    unoptimized: process.env.IMAGE_UNOPTIMIZE == "true",
-    remotePatterns: [
-      {
-        protocol: "http",
-        hostname: "localhost",
-      },
-      {
-        protocol: "https",
-        hostname: "**",
-      },
-      {
-        protocol: "http",
-        hostname: "**",
-      },
-    ],
+    unoptimized: true,
   },
 };
 
